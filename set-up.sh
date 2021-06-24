58b919ab8a51ca23487c22c112d9ef6fdcae85c7
加世田怜美 <kasedareibi@MacBook-Air.local>
2021-06-25 05:56:20 +0900

    set-up.sh

--- set-up.sh
+++ set-up.sh
@@ -1,7 +1,7 @@
+#sudo apt-get update
+#sudo apt-get install -yq git python3.7 python3-venv
 #git clone 
 
-sudo apt-get update
-sudo apt-get install -yq git python3.7 python3-venv
 sudo apt-get install tmux
 
 python3 -m venv .venv
