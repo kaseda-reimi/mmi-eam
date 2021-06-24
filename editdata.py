12f57099d5e733e550708862bd21e000d71e0d8c
加世田怜美 <kasedareibi@MacBook-Air.local>
2021-06-25 04:02:35 +0900

    editdata.py

--- editdata.py
+++ editdata.py
@@ -1,30 +1,26 @@
 import numpy as np
 import function as fc
 
-max_op = 0.9
-min_op = 0
-max_ex = 30
-min_ex = 0 #０以下を評価で除外とか
-max_w2 = 0.8
-min_w2 = 0.1
-max_l1 = 0.7
-min_l1 = 0
-max_l2 = 0.9
-min_l2 = 0
+max_op = 0.9,   min_op = 0
+max_ex = 30,    min_ex = 0 #０以下を評価で除外
+max_w1 = 4.5e-6,min_w1 = 1e-6
+max_w2 = 0.8,   min_w2 = 0.1
+max_l1 = 0.7,   min_l1 = 0
+max_l2 = 0.9,   min_l2 = 0
 
 def main():
     data_r = fc.get_data_r()
     w1,L,w2,l1,l2, _,_,output_off,_, _,_,output_on,_= data_r.T
 
-    percent_w2 = (w1 - w2) / w1 /2
-    percent_l2 = (L - l1 - l2) / L
-    percent_l1 = l1 / L
+    rate_w2 = (w1-w2) / (w1*2)
+    rate_l1 = l1 / L
+    rate_l2 = (L-l1-l2) / L
     extinction_ratio = 20 * np.log10 (output_off / output_on)#消光比
 
     normalized_w1 = normalize_w1(w1)
-    normalized_w2 = normalize_w2(percent_w2)
-    normalized_l1 = normalize_l1(percent_l1)
-    normalized_l2 = normalize_l2(percent_l2)
+    normalized_w2 = normalize_w2(rate_w2)
+    normalized_l1 = normalize_l1(rate_l1)
+    normalized_l2 = normalize_l2(rate_l2)
     normalized_ex = normalize_extinction_ratio(extinction_ratio)
     normalized_op = normalize_output_off(output_off)
     
@@ -34,11 +30,9 @@ def main():
     write_data(path, data)
 
 
-
+#正規化
 def normalize_w1(w1):
-    max_w1 = 4.5e-6
-    min_w1 = 1e-6
-    normalized_w1 = (w1 - min_w1) / (max_w1-min_w1)
+    normalized_w1 = (w1 - min_w1) / (max_w1 - min_w1)
     return normalized_w1
 
 def normalize_w2(w2):
@@ -61,12 +55,18 @@ def normalize_output_off(output_off):
     normalized_op = (output_off - min_op) / (max_op - min_op)
     return normalized_op
 
+
 def write_data(path, data):
     with open(path, mode='w') as f:
         for i in range(data.shape[0]):
             for n in data[i]:
                 f.write(str(n))
             f.write('\n')
+
+#非正規化
+def denormalize_w1(normalized_w1):
+    denormalized_w1 = normalized_w1 * (max_w1 - min_w1) + min_w1
+    return denormalized_w1
 
 def denormalize_w2(normalized_w2):
     denormalized_w2 = normalized_w2 * (max_w2 - min_w2) + min_w2
