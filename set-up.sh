61f569c034b0642375c88917ac1f214cef85074e
加世田怜美 <kasedareibi@MacBook-Air.local>
2021-06-25 05:49:27 +0900

    set-up.sh

---
+++ set-up.sh
@@ -0,0 +1,20 @@
+#git clone 
+
+sudo apt-get update
+sudo apt-get install -yq git python3.7 python3-venv
+sudo apt-get install tmux
+
+python3 -m venv .venv
+
+tmux
+
+. .venv/bin/activate
+
+pip install --upgrade pip
+pip install numpy
+pip install matplotlib
+pip install tensorflow
+pip install os
+
+
+#ulimit 
